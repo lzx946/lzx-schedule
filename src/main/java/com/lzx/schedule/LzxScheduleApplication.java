46afc22cb744783eb8e7ad7d208fb8d83bd730fe
liuzongxin <liuzongxin@3tlife.cn>
2024-06-30 03:40:21 +0800

    first commit

---
+++ src/main/java/com/lzx/schedule/LzxScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.lzx.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzxScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LzxScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lzx/schedule/LzxScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzx.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LzxScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
